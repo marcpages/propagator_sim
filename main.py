d2f617c7916962943676a344473c5a6cc3181d3d
Perello-nico <perellonicolo@gmail.com>
2025-08-20 16:34:17 +0200

    temporary main

--- main.py
+++ main.py
@@ -1,106 +1,21 @@
-import logging
-import numpy as np
-
-# from propagator.geo import GeographicInfo
-# from propagator.loader.tiles import PropagatorDataFromTiles
-from propagator.functions import moist_proba_correction_1, p_time_wang
-from propagator.loader.geotiff import PropagatorDataFromGeotiffs
-from propagator.propagator import (
-    Propagator,
-    PropagatorActions,
-    PropagatorBoundaryConditions,
-)
-# from propagator.settings import PropagatorSettings
-
-from propagator.logging_config import configure_logger
-
-configure_logger()
-
-v0 = np.loadtxt("v0_table.txt")
-prob_table = np.loadtxt("prob_table.txt")
-p_veg = np.loadtxt("p_vegetation.txt")
+# %%
+from pydantic_cli import run_and_exit
+from propagator_cli.cli import PropagatorCLI
+from propagator_cli.console import (enable_bootstrap_recording,
+                                    enable_bootstrap_export)
 
 
-# settings_dict = {}
-# settings = PropagatorSettings.from_dict(settings_dict)
+# %%
+def main():
+    # record from process start + set a temporary destination
+    enable_bootstrap_recording()
+    enable_bootstrap_export(output_folder=".",
+                            basename="propagator_boot",
+                            append_timestamp=True)
 
-# Load the input data
-# settings.
+    run_and_exit(PropagatorCLI)
 
 
-# if settings.run_from_tiles:
-#     ...
-#     loader = PropagatorDataFromTiles(...)
-# else:
-#     ...
-#     loader = PropagatorDataFromGeotiffs(...)
-
-loader = PropagatorDataFromGeotiffs(
-    dem_file="example/dem_clip.tif",
-    veg_file="example/veg_clip.tif",
-)
-
-# Load the data
-dem = loader.get_dem()
-veg = loader.get_veg()
-geo_info = loader.get_geo_info()
-
-simulator = Propagator(
-    dem=dem,
-    veg=veg,
-    realizations=1,
-    ros_0=v0,
-    probability_table=prob_table,
-    veg_parameters=p_veg,
-    do_spotting=False,
-    p_time_fn=p_time_wang,
-    p_moist_fn=moist_proba_correction_1,
-)
-
-ignition_array = np.zeros(dem.shape, dtype=np.uint8)
-ignition_array[100:101, 100:101] = 1
-
-boundary_conditions_list: list[PropagatorBoundaryConditions] = [
-    PropagatorBoundaryConditions(
-        time=0,
-        ignitions=ignition_array,
-        wind_speed=np.ones(dem.shape) * 10,
-        wind_dir=np.ones(dem.shape) * 180,
-        moisture=np.ones(dem.shape) * 0.05,
-    ),
-]
-actions_list: list[PropagatorActions] = []
-
-time_resolution = 60
-time_limit = 3600
-
-while True:
-    next_time = simulator.next_time()
-    if next_time is None:
-        break
-
-    logging.info(f"Supposed Next time: {next_time}")
-
-    if len(boundary_conditions_list) > 0:
-        boundary_conditions = boundary_conditions_list[0]
-        if boundary_conditions.time <= next_time:
-            simulator.set_boundary_conditions(boundary_conditions)
-            boundary_conditions_list.pop(0)
-
-    if len(actions_list) > 0:
-        actions = actions_list[0]
-        if actions.time <= next_time:
-            simulator.apply_actions(actions)
-            actions_list.pop(0)
-
-    logging.info(f"Current time: {simulator.time}")
-    simulator.step()
-    logging.info(f"New time: {simulator.time}")
-
-    if simulator.time % time_resolution == 0:
-        output = simulator.get_output()
-        # Save the output to the specified folder
-        ...
-
-    if simulator.time > time_limit:
-        break
+# %%
+if __name__ == "__main__":
+    main()
